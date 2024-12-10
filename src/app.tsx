b8bfebb08681f8eee29081a040c6373451f01b64
trannhon70 <kevintran351996@gmail.com>
2024-12-10 17:02:30 +0700

    firt commit

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+// Modified from the create-react-app TypeScript template
+// See: https://github.com/facebook/create-react-app/blob/main/packages/cra-template-typescript/template/src/App.tsx
+import React from 'react';
+
+import './app.css';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app" role="main">
+       
+      </div>
+    );
+  }
+}
