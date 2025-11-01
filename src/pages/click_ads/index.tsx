1e3af4385914bbdd7c74323d667fe904fe7b6d59
trannhon70 <kevintran351996@gmail.com>
2025-11-01 14:07:54 +0700

    update

--- src/pages/click_ads/index.tsx
+++ src/pages/click_ads/index.tsx
@@ -1,11 +1,10 @@
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
 import type { FormProps } from 'antd';
 import { Button, Col, Form, Input, Row, Space } from "antd";
 import type { FC } from "react";
 import { useState } from "react";
-import { FaPlay, FaRegStopCircle } from "react-icons/fa";
-import { proxyAPI } from '../../apis/proxy.api';
+import { FaPlay } from "react-icons/fa";
 import { commandApi } from '../../apis/command.api';
-
 type FieldType = {
     keyword?: string;
     domain?: string;
@@ -31,9 +30,7 @@ const ComponentClickAds: FC = () => {
         console.log('Failed:', errorInfo);
     };
 
-    const onClickPrev = () => {
 
-    }
     return <>
         <div className="flex items-center justify-center font-semibold text-2xl uppercase text-green-600" >
             Thực hiện click ads
@@ -57,48 +54,95 @@ const ComponentClickAds: FC = () => {
                     <Col span={24}>
                         <Row gutter={{ md: 24 }}>
                             <Col span={12}>
-                                <Form.Item<FieldType>
-                                    label="Từ khóa tìm kiếm :"
-                                    name="keyword"
-                                    rules={[
-                                        { required: true, message: 'từ khóa không được bỏ trống!' }
-                                    ]}
+                                <Form.Item
+                                    label="Từ khóa tìm kiếm:"
+                                    required
                                 >
-                                    <Space.Compact style={{ width: '100%' }} >
-                                        <Input size="large" style={{ width: '80%' }} defaultValue="" />
-                                        <Input size="large" style={{ width: '20%' }} defaultValue="key" />
-                                    </Space.Compact>
+                                    <Form.List
+                                        name="keywords"
+                                        initialValue={[""]}
+                                        rules={[
+                                            {
+                                                validator: async (_, names) => {
+                                                    if (!names || names.length < 1) {
+                                                        return Promise.reject(new Error("Phải có ít nhất 1 từ khóa!"));
+                                                    }
+                                                },
+                                            },
+                                        ]}
+                                    >
+                                        {(fields, { add, remove }) => (
+                                            <div className="w-[100%]">
+                                                {fields.map(({ key, name, ...restField }) => (
+                                                    <div className="flex items-start gap-2.5 justify-between" >
+                                                        <Form.Item
+                                                            style={{ width: '100%' }}
+                                                            className="w-[100%]"
+                                                            {...restField}
+                                                            name={name}
+                                                            rules={[
+                                                                { required: true, message: "Không được để trống!" },
+                                                            ]}
+                                                        >
+                                                            <Input style={{ width: '100%' }} className="w-[100%]" placeholder="Nhập từ khóa..." />
+                                                        </Form.Item>
+
+                                                        {fields.length > 1 && (
+                                                            <MinusCircleOutlined
+                                                                onClick={() => remove(name)}
+                                                                style={{ color: "red", marginTop: '10px' }}
+                                                                width={100}
+                                                            />
+                                                        )}
+                                                    </div>
+                                                ))}
+
+                                                <Form.Item>
+                                                    <Button
+                                                        type="dashed"
+                                                        onClick={() => add()}
+                                                        icon={<PlusOutlined />}
+                                                        block
+                                                    >
+                                                        Thêm từ khóa
+                                                    </Button>
+                                                </Form.Item>
+                                            </div>
+                                        )}
+                                    </Form.List>
                                 </Form.Item>
                             </Col>
                             <Col span={12}>
-                                <Form.Item<FieldType>
-                                    label="Nhập domain :"
-                                    name="domain"
-                                    rules={[
-                                        { required: true, message: 'Domain không được bỏ trống!' },
-                                    ]}
+                                <Col span={24}>
+                                    <Form.Item<FieldType>
+                                        label="Nhập domain :"
+                                        name="domain"
+                                        rules={[
+                                            { required: true, message: 'Domain không được bỏ trống!' },
+                                        ]}
 
-                                >
-                                    <Space.Compact style={{ width: '100%' }} >
-                                        <Input size="large" style={{ width: '80%' }} defaultValue="" />
-                                        <Input size="large" style={{ width: '20%' }} defaultValue="web" />
-                                    </Space.Compact>
-                                </Form.Item>
-                            </Col>
-                            <Col span={12}>
-                                <Form.Item<FieldType>
-                                    label="Nhập số lần click :"
-                                    name="quantity"
-                                    rules={[
-                                        { required: true, message: 'số lần click không được bỏ trống!' },
-                                    ]}
+                                    >
+                                        <Space.Compact style={{ width: '100%' }} >
+                                            <Input size="large" style={{ width: '80%' }} defaultValue="" />
+                                            <Input size="large" style={{ width: '20%' }} defaultValue="web" />
+                                        </Space.Compact>
+                                    </Form.Item>
+                                </Col>
+                                <Col span={24}>
+                                    <Form.Item<FieldType>
+                                        label="Nhập số lần click :"
+                                        name="quantity"
+                                        rules={[
+                                            { required: true, message: 'số lần click không được bỏ trống!' },
+                                        ]}
 
-                                >
-                                    <Space.Compact style={{ width: '100%' }} >
-                                        <Input size="large" style={{ width: '80%' }} defaultValue="" />
-                                        <Input size="large" style={{ width: '20%' }} defaultValue="lần" />
-                                    </Space.Compact>
-                                </Form.Item>
+                                    >
+                                        <Space.Compact style={{ width: '100%' }} >
+                                            <Input size="large" style={{ width: '80%' }} defaultValue="" />
+                                            <Input size="large" style={{ width: '20%' }} defaultValue="lần" />
+                                        </Space.Compact>
+                                    </Form.Item>
+                                </Col>
                             </Col>
 
                             <Col span={24}>
@@ -106,9 +150,9 @@ const ComponentClickAds: FC = () => {
                                     <Button loading={loading} icon={<FaPlay />} color="primary" variant="solid" htmlType="submit">
                                         Chạy
                                     </Button>
-                                    <Button icon={<FaRegStopCircle color='red' />} onClick={onClickPrev} className="ml-2.5" color="orange" variant="solid" type="default">
+                                    {/* <Button icon={<FaRegStopCircle color='red' />} onClick={onClickPrev} className="ml-2.5" color="orange" variant="solid" type="default">
                                         Dừng
-                                    </Button>
+                                    </Button> */}
                                 </Form.Item>
                             </Col>
                         </Row>
